1700708af7e36425775b6ee89b6a7711ea3d57da
dhjoe <ssd351@daum.net>
2022-12-26 11:48:05 +0900

    테스트

---
+++ Boot01_Hello2/src/main/java/com/example/demo/auto/Car.java
@@ -0,0 +1,10 @@
+package com.example.demo.auto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+	public void drive() {
+		System.out.println("달려요!");
+	}
+}
---
+++ Spring04_Final/src/main/java/com/gura/spring04/exception/ExceptionController.java
@@ -0,0 +1,10 @@
+package com.gura.spring04.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+/*
+ * Spring MVC가 동작 중 특정 type의 예외가 발생하면 해당 예외를 여기서 처리 할 수 있다.
+ */
+@ControllerAdvice
+public class ExceptionController {
+	
+}
