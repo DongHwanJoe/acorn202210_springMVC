63b212b4d64287301544e848b5d0a194562f95cb
dhjoe <ssd351@daum.net>
2023-01-02 11:03:05 +0900

    Boot07 프로젝트 추가 및 기본구성

---
+++ Boot07_Final/src/main/java/com/gura/boot07/HomeController.java
@@ -0,0 +1,26 @@
+package com.gura.boot07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(HttpServletRequest request) {
+		
+		List<String> noticeList = new ArrayList<>();
+		noticeList.add("Spring Boot Start");
+		noticeList.add("asd");
+		noticeList.add("zxc");
+		
+		request.setAttribute("noticeList", noticeList);
+		
+		return "home";
+	}
+}
